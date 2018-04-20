616712cf6532e9330bb728ac2a948c891fe652a6
Ivan Fraixedes <ivan.fraixedes@cycloid.io>
2018-04-20 08:54:44 +0000

    state-updater: Document the exported funcs and iface
    Add the JSDoc comments to the exported function and the interface of the
    returned object by those functions.

--- src/state-updater.js
+++ src/state-updater.js
@@ -1,4 +1,53 @@
+/**
+ * The state-updater-on-actions instance.
+ * @typedef {Object} StateUpdaterOnActions
+ * @method call - Calls an action with the passed parameters.
+ *    @param {Action} action - The action to call.
+ *    @param {*} params - The parameters to pass to the action when it will be
+ *        called.
+ *    @return {Promise} - The promise that the action returns, or if it returns
+ *        a promise with the value returned by the 'updateState', it will call
+ *        the 'updateAction' and if it returns a rejected promise, then it will
+ *        be returned such rejected promise.
+ * @method callCtx - Calls an action using the specified context
+ *    (e.g obj.action()) and the passed parameters.
+ *    @param {Action} action - The action to call.
+ *    @param {*} ctx - The context to use for calling the action (i.e. `this`
+ *        pointer).
+ *    @param {*} params - The parameters to pass to the action when it will be
+ *        called.
+ *    @return {Promise} - The promise that the action returns, or if it returns
+ *        a promise with the value returned by the 'updateState', it will call
+ *        the 'updateAction' and if it returns a rejected promise, then it will
+ *        be returned such rejected promise.
+ * @method updateState - returns the value which the actions must resolve their
+ *    returned promises when they require an state update.
+ *    @return {*} - The value DOES NOT matter as it's used internally to identify
+ *        that the update state action must be called.
+ */
 
+/**
+ * Action is a function which returns a promise; in order to notify that an
+ * update state is required it MUST return a resolved promise which the value
+ * returned by the StateUpdaterOnActions~updateState method.
+ * @async
+ * @typedef {Function} Action
+ */
+
+/**
+ * Create an instance of state updater that uses an action function, which
+ * doesn't require to be executed in any context (e.g. a method of an
+ * object, or a function which use `this` pointer) and requires the passed
+ * parameters.
+ *
+ * @param {Function} updateAction - The function which update the shared state
+ *    require by the actions which will be executed by the returned
+ *    state-updater-on-actions instance. The function MUST return a promise.
+ * @param {*} params - The parameters to pass to the 'updateAction' each time
+ *    that will be executed.
+ * @return {StateUpdaterOnActions} - The instance which will call the
+ *    'updateAction' with the passed 'params' when required.
+ */
 export function createStateUpdater (updateAction, ...params) {
   if (typeof updateAction !== 'function') {
     throw new TypeError('updateAction must be a function')
@@ -7,6 +56,21 @@ export function createStateUpdater (updateAction, ...params) {
   return create(updateAction, null, ...params)
 }
 
+/**
+ * Create an instance of state updater that uses an action function, which
+ * requires to be executed in any context (e.g. a method of an object, or a
+ * function which use `this` pointer) and requires the passed parameters.
+ *
+ * @param {Function} updateAction - The function which update the shared state
+ *    require by the actions which will be executed by the returned
+ *    state-updater-on-actions instance. The function MUST return a promise.
+ * @param {*} ctx - The context to use for calling the action (i.e. `this`
+ *        pointer).
+ * @param {*} params - The parameters to pass to the 'updateAction' each time
+ *    that will be executed.
+ * @return {StateUpdaterOnActions} - The instance which will call the
+ *    'updateAction' with the passed 'params' when required.
+ */
 export function createStateUpdaterCtx (updateAction, ctx, ...params) {
   if (typeof updateAction !== 'function') {
     throw new TypeError('updateAction must be a function')
